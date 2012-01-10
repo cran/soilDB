40414836d2c3143d65114c8d6d85ce8b54d40d4d
D.E. Beaudette <dylan.beaudette@ca.usda.gov>
2012-01-10 00:00:00 +0000

    version 0.3-3

--- R/get_hz_data_from_NASIS_db.R
+++ R/get_hz_data_from_NASIS_db.R
@@ -1,9 +1,8 @@
-get_hz_data_from_NASIS_db <- function(dsn)
-  {
-  q <- "SELECT pedon.peiid, phorizon.phiid, pedon.upedonid as pedon_id,
+get_hz_data_from_NASIS_db <- function(dsn) {
+	q <- "SELECT pedon.peiid, phorizon.phiid, pedon.upedonid as pedon_id,
   phorizon.hzname, phorizon.hzdept, phorizon.hzdepb,
-  phorizon.claytotest as clay, phorizon.silttotest as silt, phorizon.sandtotest as sand, CASE WHEN tx.ChoiceName IS NULL THEN til.ChoiceName  ELSE tx.ChoiceName END as texture_class, tmod.ChoiceName as texture_modifier, phfield, eff.ChoiceName AS effervescence, l.labsampnum, CASE WHEN f.total_frags_pct IS NULL THEN 0 ELSE f.total_frags_pct END AS total_frags_pct
-  FROM (((((((
+  phorizon.claytotest as clay, phorizon.silttotest as silt, phorizon.sandtotest as sand, CASE WHEN tx.ChoiceName IS NULL THEN til.ChoiceName  ELSE tx.ChoiceName END as texture_class, phfield, eff.ChoiceName AS effervescence, l.labsampnum, CASE WHEN f.total_frags_pct IS NULL THEN 0 ELSE f.total_frags_pct END AS total_frags_pct
+  FROM (((((
   (dbo.pedon INNER JOIN dbo.phorizon ON dbo.pedon.peiid = dbo.phorizon.peiidref) 
   LEFT OUTER JOIN (SELECT phiidref, SUM(fragvol) as total_frags_pct 
     FROM dbo.phfrags
@@ -12,24 +11,24 @@ get_hz_data_from_NASIS_db <- function(dsn)
   LEFT OUTER JOIN (SELECT phtiid, phiidref, texcl, lieutex FROM dbo.phtexture) as t ON dbo.phorizon.phiid = t.phiidref)
   LEFT OUTER JOIN (SELECT * FROM dbo.MetadataDomainDetail WHERE dbo.MetadataDomainDetail.DomainID = 189) AS tx ON t.texcl = tx.ChoiceValue)
   LEFT OUTER JOIN (SELECT * FROM dbo.MetadataDomainDetail WHERE dbo.MetadataDomainDetail.DomainID = 192) AS til ON t.lieutex = til.ChoiceValue)
-  LEFT OUTER JOIN (SELECT phtiidref, texmod FROM dbo.phtexturemod) as tm ON t.phtiid = tm.phtiidref)
-  LEFT OUTER JOIN (SELECT * FROM dbo.MetadataDomainDetail WHERE dbo.MetadataDomainDetail.DomainID = 190) AS tmod ON tm.texmod = tmod.ChoiceValue)
   LEFT OUTER JOIN (SELECT * FROM dbo.MetadataDomainDetail WHERE dbo.MetadataDomainDetail.DomainID = 1255) AS eff ON dbo.phorizon.effclass = eff.ChoiceValue
   ORDER BY pedon.upedonid, phorizon.hzdept ASC;"
-  
-  # setup connection to our pedon database
-  channel <- odbcConnect('nasis_local', uid='NasisSqlRO', pwd='Re@d0n1y')
-
-  # exec query
-  cat(paste('fetching from', 'local NASIS database', '...\n'))
-  d <- sqlQuery(channel, q, stringsAsFactors=FALSE)
-  
-  ## TODO test for non-uniqueness
-  
-  # close connection
-  odbcClose(channel)
-  
-  # done
-  return(d)
-  }
+	
+	# setup connection to our pedon database
+	channel <- odbcConnect('nasis_local', uid='NasisSqlRO', pwd='Re@d0n1y')
+	
+	# exec query
+	d <- sqlQuery(channel, q, stringsAsFactors=FALSE)
+	
+	# test for duplicate horizons: due to bugs in our queries that lead to >1 row/hz
+	hz.tab <- table(d$phiid)
+	if(any(hz.tab > 1))
+		cat('notice: duplicate horizons in query results\n')
+	
+	# close connection
+	odbcClose(channel)
+	
+	# done
+	return(d)
+}
 
